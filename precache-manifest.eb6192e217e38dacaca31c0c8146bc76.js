00a653f93b7d42f1abf46a4a0beeffe577fd8bdf
brunocasanova <deftonicks_me@hotmail.com>
2019-12-02 19:52:21 +0000

    Updates

---
+++ precache-manifest.eb6192e217e38dacaca31c0c8146bc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3149824634bae44367138e648a7214b",
+    "url": "/pages/index.html"
+  },
+  {
+    "revision": "4f28e66f41c01804b3ee",
+    "url": "/pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "151b50fbbcb15d1243e2",
+    "url": "/pages/static/js/2.ba9ffee8.chunk.js"
+  },
+  {
+    "revision": "4f28e66f41c01804b3ee",
+    "url": "/pages/static/js/main.429d48f2.chunk.js"
+  },
+  {
+    "revision": "50d356e1e5b510dfcdab",
+    "url": "/pages/static/js/runtime-main.0b35eaf1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pages/static/media/logo.25bf045c.svg"
+  }
+]);
